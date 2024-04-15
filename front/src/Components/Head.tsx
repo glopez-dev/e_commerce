4fd43eef64608fa7262e7fef01325ce4360fd524
ellidaan <elone.meimoun@epitech.eu>
2024-04-15 10:42:42 +0200

    feat (front): Initialisation du projet

---
+++ front/src/Components/Head.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Head extends Component {
+    render() {
+        return (
+            <div>
+                <p>coucouc</p>
+            </div>
+        );
+    }
+}
+
+export default Head;
---
+++ front/src/Pages/home.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Head from '../Components/Head';
+
+class home extends Component {
+    render() {
+        return (
+            <div>
+                <Head />
+
+                <p>salut</p>
+
+            </div>
+        );
+    }
+}
+
+export default home;
