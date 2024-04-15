3b81cf32a72859ff99420438935c0c430c9b4f06
ellidaan <elone.meimoun@epitech.eu>
2024-04-15 22:00:34 +0200

    feat(front):head

--- front/src/Components/Head.tsx
+++ front/src/Components/Head.tsx
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Style from '../Styles/Header.module.css';
+import vinted from '../assets/Vinted.png';
 
-class Head extends Component {
-    render() {
-        return (
-            <div>
-                <p>coucouc</p>
+const Head = () => {
+    return (
+
+        <div className={Style.ctn1}>
+
+            <div className={Style.ctn2}>
+
+                <div className={Style.gauche}>
+
+                    <p>menu</p>
+                    <p>Recherche</p>
+                    <div />
+
+                </div>
+
+                <div className={Style.mid}>
+
+                    <img className={Style.logo} src={vinted} />
+
+                </div>
+
+                <div className={Style.droite}>
+
+                    <p>contactez-nous</p>
+                    <p>WhishList</p>
+                    <div />
+
+                </div>
+
+
+
+
             </div>
-        );
-    }
-}
+
+        </div>
+
+
+
+    );
+};
 
 export default Head;
