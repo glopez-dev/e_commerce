47491a54fea43058ca02dd5a07eee23f7760c2af
Gabriel Lopez <91618272+glopez-dev@users.noreply.github.com>
2024-04-18 19:48:02 +0200

    Add the update and delete methods for the Product entity (#31)
    * feat( ProductController.php ): Add the `getProduct()` method
    
    * fix( ProductController.php ): Replace controller return type from json to JsonResponse.
    
    * fix( templates/ ): Delete unused template as the backend is a Json API.
    
    * fix: Restore lost files after revert.
    
    * fix( .env.test): Add the environment variables needed to generate the JWT keypair.
    
    * fix( ProductController.php ): Fix the serialization to json by using the $this->json Controller method as it automatically creates aJsonResponse and serializes it, calling direct new JsonResponse doesn't uses the serializer service.
    
    * fix( .env.test ): Fix bad formatted  comment.

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -10,12 +10,13 @@ use App\Entity\Product;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Exception\ORMException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api')]
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'create_product', methods: ['POST'])]
-    public function createProduct(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    public function createProduct(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
@@ -29,9 +30,9 @@ class ProductController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
 
-            return new JsonResponse($product, Response::HTTP_CREATED);
+            return $this->json($product, Response::HTTP_CREATED);
         } catch (ORMException $exception) {
-            return new JsonResponse(['error' => 'Cannot create product'], Response::HTTP_BAD_REQUEST);
+            return $this->json(['error' => 'Cannot create product'], Response::HTTP_BAD_REQUEST);
         }
     }
 
@@ -40,7 +41,7 @@ class ProductController extends AbstractController
     {
         $products = $entityManager->getRepository(Product::class)->findAll();
 
-        return new JsonResponse($products, Response::HTTP_OK);
+        return $this->json($products, Response::HTTP_OK);
     }
 
     #[Route('/product/{productId}', name: 'get_product', methods: ['GET'])]
@@ -48,6 +49,49 @@ class ProductController extends AbstractController
     {
         $product = $entityManager->getRepository(Product::class)->find($request->get('productId'));
 
-        return new JsonResponse($product, Response::HTTP_OK);
+        return $this->json($product, Response::HTTP_OK);
+    }
+
+    #[Route('/product/{id}', name: 'update_product', methods: ['PUT'])]
+    public function updateProduct(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $product = $entityManager->getRepository(Product::class)->find($request->get('id'));
+        if (!$product) {
+            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $product->setName($data['name'])
+            ->setDescription($data['description'])
+            ->setPhoto($data['photo'])
+            ->setPrice($data['price']);
+
+        try {
+            $entityManager->persist($product);
+            $entityManager->flush();
+        } catch (ORMException $exception) {
+            return $this->json(['error' => 'Cannot update product'], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->json($product, Response::HTTP_OK);
+    }
+
+    #[Route('/product/{id}', name: 'delete_product', methods: ['DELETE'])]
+    public function deleteProduct(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $product = $entityManager->getRepository(Product::class)->find($request->get('id'));
+        if (!$product) {
+            return $this->json(['error' => 'Product not found'], Response::HTTP_NOT_FOUND);
+        }
+
+        try {
+            $entityManager->remove($product);
+            $entityManager->flush();
+        } catch (ORMException $exception) {
+            return $this->json(['error' => 'Cannot delete product'], Response::HTTP_BAD_REQUEST);
+        }
+
+        return $this->json(['message' => 'Product deleted successfully'], Response::HTTP_ACCEPTED);
     }
 }
